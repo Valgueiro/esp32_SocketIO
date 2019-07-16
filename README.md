3720e041917ebece719d32406b59ac3fb4d841f4
Mateus Valgueiro <mvalgueiro@apple.com>
2019-07-16 16:18:20 -0300

    Add more information on README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # esp32_SocketIO
+
+Simple example using the [socketIO](https://socket.io/) library on ESP 32 boards.
---
+++ socketIO.ino
@@ -0,0 +1,65 @@
+// remeber to add the last version (master) from repository:
+// https://github.com/timum-viw/socket.io-client
+
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <SocketIoClient.h>
+
+#define USE_SERIAL Serial
+
+WiFiMulti WiFiMulti;
+SocketIoClient webSocket;
+
+// CONST VARIABLES
+const char *ssid = "<INSERT HERE WIFI SSID>";
+const char *pass = "<INSERT HERE WIFI PASSWORD>";
+const char *HOST = "<INSERT HERE HOST ADDRESS>";
+
+void event(const char *payload, size_t length){
+    USE_SERIAL.printf("got message: %s\n", payload);
+}
+
+void setup(){
+    USE_SERIAL.begin(9600);
+
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for (uint8_t t = 4; t > 0; t--){
+        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }   
+
+
+    // Connect to WIFI
+    WiFiMulti.addAP(ssid, pass);
+
+    while (WiFiMulti.run() != WL_CONNECTED){
+        delay(100);
+    }
+
+    // Receive events from server
+    webSocket.on("event", event);
+
+    webSocket.begin(HOST);
+}
+
+int count = 0;
+
+void loop(){
+    webSocket.loop();
+    count++;
+    if (count == 18000){
+        count = 0;
+        
+        // Send data to Server
+        webSocket.emit("status", "Hello from esp32!");
+    }
+}
